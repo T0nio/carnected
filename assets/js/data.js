3ac1a400ff06be6b832bb87ccf6d6b70a357bc65
La Gonz <main@toni0.com>
2015-11-28 14:47:56 +0100

    Adding categorys click feature

--- assets/js/data.js
+++ assets/js/data.js
@@ -4,17 +4,30 @@ var categories = {
 		icon: "fa-cutlery",
 		places: [
 			{
-				title: "",
-				description: "",
-				lat: "",
-				lng: ""
-			}
+				title: "Le bohneur d'Antony",
+				description: "La nourriture du soleil levant.",
+				lat: 48.76104,
+				lng: 2.29132
+			},
+			{
+				title: "Le bohneur d'Antony",
+				description: "La nourriture du soleil levant.",
+				lat: 48.76104,
+				lng: 2.29132
+			},
 		]
 	},
 	"bar": {
 		label: "bar",
 		icon: "fa-glass",
-		places: []
+		places: [
+			{
+				title: "Brasserie de l'école Centrale",
+				description: "Dépot de bilan, déso.",
+				lat: 48.76122,
+				lng: 2.29032
+			},
+		]
 	},
 	"shopping": {
 		label: "shopping",
