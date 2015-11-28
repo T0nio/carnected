6e5822b502f00c9821fb47e528b8b96a961dba94
gagnadref <florian.gagnadre@student.ecp.fr>
2015-11-28 13:28:24 +0100

    Categories in menu are dynamically constructed from data

---
+++ assets/js/data.js
@@ -0,0 +1,29 @@
+var categories = [
+	{
+		label: "restaurant",
+		icon: "fa-cutlery",
+		places: [
+			{
+				title: "",
+				description: "",
+				lat: "",
+				lng: ""
+			}
+		]
+	},
+	{
+		label: "bar",
+		icon: "fa-glass",
+		places: []
+	},
+	{
+		label: "shopping",
+		icon: "fa-shopping-bag",
+		places: []
+	},
+	{
+		label: "amis",
+		icon: "fa-users",
+		places: []
+	}
+]
